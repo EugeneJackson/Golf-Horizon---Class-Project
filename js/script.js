07447730d9c83239bbc61d566976734ddff949ee
euyin <alexander.simonszbacha@gmail.com>
2026-05-27 01:44:14 +0200

    Implementación de delta time

--- js/script.js
+++ js/script.js
@@ -21,14 +21,16 @@ class AgujeroNegro {
     cVelocity;
     massivePosY;
     massivePosX;
+    radioVisualAgujeroNegro;
 
-    constructor(constGravitacionalUniversal, masaAgujeroNegro, cVelocity, massivePosX, massivePosY) {
+    constructor(constGravitacionalUniversal, masaAgujeroNegro, cVelocity, massivePosX, massivePosY, radioVisualAgujeroNegro) {
         this.constGravitacionalUniversal = constGravitacionalUniversal;
         this.masaAgujeroNegro = masaAgujeroNegro;
         this.cVelocity = cVelocity;
         this.massivePosX = massivePosX;
         this.massivePosY = massivePosY;
         this.radioHorizonte = (2 * this.constGravitacionalUniversal * this.masaAgujeroNegro) / Math.pow(this.cVelocity, 2);
+        this.radioVisualAgujeroNegro = radioVisualAgujeroNegro;
     } 
 }
 
@@ -45,17 +47,22 @@ var x_massive = c.width / 2;
 var y_massive = c.height / 2;
 
 
-const massiveObj = new AgujeroNegro(100, 1500, 50, x_massive, y_massive);
+const massiveObj = new AgujeroNegro(10000, 5000, 50, x_massive, y_massive, 100);
 const ball = new Bola(c.width / 5, c.height / 1.3, 0, 0, 10);
 
-function gameLoop() {
+var tiempoAnterior = 0;
+
+
+function gameLoop(tiempoActual) {
+
+    var dt = (tiempoActual - tiempoAnterior) / 1000;
+    tiempoAnterior = tiempoActual;
 
     ctx.clearRect(0, 0, c.width, c.height);
-    actualizarFisica();
+    actualizarFisica(dt);
     dibujarAgujeroNegro();
     dibujarBola();
     requestAnimationFrame(gameLoop);
-
 }
 
 
@@ -71,7 +78,7 @@ function dibujarAgujeroNegro() {
     * 3 parametro: radio del circulo.
     * 4 y 5 parametro: ángulo de inicio y fin.
     */
-    ctx.arc(massiveObj.massivePosX, massiveObj.massivePosY, massiveObj.radioHorizonte, 0, 2 * Math.PI);
+    ctx.arc(massiveObj.massivePosX, massiveObj.massivePosY, massiveObj.radioVisualAgujeroNegro, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
 }
@@ -86,7 +93,8 @@ function dibujarBola() {
 
 }
 
-function actualizarFisica() {
+function actualizarFisica(dt) {
+
     //Diferencia de posición en X e Y entre la bola y el agujero negro.
     var dx = massiveObj.massivePosX - ball.bola_x;
     var dy = massiveObj.massivePosY - ball.bola_y;
@@ -104,13 +112,13 @@ function actualizarFisica() {
     //===================================================================================================
     
     //bola_vx/vy = Veloicdad actual de la bola en X e Y - se acumula cada frame
-    ball.bola_vx += ax;
-    ball.bola_vy += ay;
+    ball.bola_vx += ax * dt;
+    ball.bola_vy += ay * dt;
 
     //Se actualiza la posición de la bola en X e Y con la velocidad actual acumulada en cada frame.
-    ball.bola_x += ball.bola_vx;
-    ball.bola_y += ball.bola_vy;
+    ball.bola_x += ball.bola_vx * dt;
+    ball.bola_y += ball.bola_vy * dt;
 
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
